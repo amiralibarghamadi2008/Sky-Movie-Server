307f0fc3fa6593549911eebe3a260a1ebe47f978
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-20 15:23:30 +0330

    rename file to SendSmsService.js and macke VerifySmsService.js to verify otp code

---
+++ src/service/SmsService/VerifySmsService.js
@@ -0,0 +1,23 @@
+import { GetOtpCode } from "../../repository/OtpRepository/OtpRepository.js";
+
+export default async function Ve(params) {
+  try {
+    const { phoneNumber, userCode } = verifyData;
+
+    const otpRecord = await GetOtpCode(phoneNumber);
+
+    if (!otpRecord) {
+      throw new Error(
+        "کد تایید منقضی شده یا وجود ندارد. لطفاً مجدداً تلاش کنید.",
+      );
+    }
+
+    if (otpRecord.code !== String(userCode)) {
+      throw new Error("کد تایید وارد شده اشتباه است.");
+    }
+
+    return { message: "احراز هویت با موفقیت انجام شد." };
+  } catch (err) {
+    throw err;
+  }
+}
