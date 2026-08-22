ff5b1816ab05a42de50d4ff9c0fab07a09f315fe
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-22 21:43:18 +0330

    macke access token file for genrate access token

---
+++ src/utils/tokens/AccessToken/accessToken.js
@@ -0,0 +1,14 @@
+import "dotenv/config"
+import jwt from "jsonwebtoken"
+
+export default async function AccessToken(res , userData) {
+    try{
+        const accessToken = jwt.sign(
+            {userId : userData._id ,firstName : userData.firstName ,userRole : userData.role ,jti : crypto.randomUUID()},
+            process.env.Access_Token_Security_Code,
+            {expiresIn : "10m"}
+        )
+    }catch(err){
+        throw new Error(`${err.message}`)
+    }
+}
