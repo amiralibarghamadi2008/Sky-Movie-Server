5997b5f39fc3166690f14793067ec1637a463c32
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-30 16:08:47 +0330

    add authLimiter middleware for rate limiting

---
+++ src/middleware/RateLimit/authLimiter.js
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+const authLimiter = rateLimit({
+  windowMs: 2 * 60 * 1000, 
+  max: 5,
+  message: {
+    success: false,
+    message:
+      "تعداد درخواست‌های شما بیش از حد مجاز است. لطفاً ۲ دقیقه دیگر تلاش کنید.",
+  },
+  standardHeaders: true, 
+  legacyHeaders: false, 
+});
+
+export default authLimiter
