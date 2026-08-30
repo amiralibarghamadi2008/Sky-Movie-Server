31c06d2b4f01c4adbda2f8035e0ff2e23b208503
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-30 17:59:37 +0330

    add download link to episode schema

--- src/model/EpisodeModel/episode.js
+++ src/model/EpisodeModel/episode.js
@@ -1,35 +1,57 @@
 import mongoose from "mongoose";
 
+const DownloadLinkSchema = new mongoose.Schema({
+  quality: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  fileSize: {
+    type: String,
+    trim: true,
+  },
+  directDownloadLink: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
 const EpisodeSchema = new mongoose.Schema(
-    {
-        titleEpisode : {
-           type : String,
-            required: true,
-            trim : true
-        },
-        seasonNumber : {
-           type : Number,
-            required: true,
-        },
-        episodeNumber : {
-           type : Number,
-            required: true,
-        },
-        duration : {
-           type : String,
-            required: true,
-        },
-        series : {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "series",
-            required : true
-        }
+  {
+    titleEpisode: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps : true,
-    }
-)
+    seasonNumber: {
+      type: Number,
+      required: true,
+    },
+    episodeNumber: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    series: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "series",
+      required: true,
+    },
+    downloadLinks: {
+      type: [DownloadLinkSchema],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const EpisodeModel = mongoose.models.episode || mongoose.model("episode" , EpisodeSchema)
+const EpisodeModel =
+  mongoose.models.episode || mongoose.model("episode", EpisodeSchema);
 
-export default EpisodeModel
+export default EpisodeModel;
