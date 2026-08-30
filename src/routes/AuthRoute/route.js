d37bbdd26e8f8014ab6d97e3f232f3b6cd3f2c9c
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-30 12:12:45 +0330

    add sign-out route and connect to server this api too

--- src/routes/AuthRoute/route.js
+++ src/routes/AuthRoute/route.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import SignInController from "../../controller/AuthControllers/SignInController/signInController.js";
+import signOutController from "../../controller/AuthControllers/SignOutController/signOutController.js";
 
 const route = express.Router()
 
-route.post("/sign-in" , SignInController)
+route.post("/auth/sign-in" , SignInController)
+route.post("/auth/sign-out" , signOutController)
 
 export default route
