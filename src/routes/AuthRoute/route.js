7422d93ef41921ae33dc62694a0d5731191ee428
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-08-30 16:15:04 +0330

    use authLimiter middleware for my route and api's

--- src/routes/AuthRoute/route.js
+++ src/routes/AuthRoute/route.js
@@ -2,10 +2,11 @@ import express from "express";
 
 import SignInController from "../../controller/AuthControllers/SignInController/signInController.js";
 import signOutController from "../../controller/AuthControllers/SignOutController/signOutController.js";
+import authLimiter from "../../middleware/RateLimit/authLimiter.js";
 
 const route = express.Router()
 
-route.post("/auth/sign-in" , SignInController)
+route.post("/auth/sign-in", authLimiter , SignInController)
 route.post("/auth/sign-out" , signOutController)
 
 export default route
--- src/routes/SmsRoute/route.js
+++ src/routes/SmsRoute/route.js
@@ -2,10 +2,11 @@ import express from "express";
 
 import SmsController from "../../controller/SmsControllers/SendSmsController/SendSmsController.js";
 import Verify_Controllers from "../../controller/SmsControllers/VerifySmsController/VerifySmsController.js";
+import authLimiter from "../../middleware/RateLimit/authLimiter.js";
 
 const route = express.Router()
 
-route.post("/send-sms" , SmsController)
-route.get("/verify-sms" , Verify_Controllers)
+route.post("/send-sms" , authLimiter ,SmsController)
+route.post("/verify-sms" , authLimiter ,Verify_Controllers)
 
 export default route
