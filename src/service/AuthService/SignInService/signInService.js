b0011be9ea5083fe40c16e62f37b756134f95940
amiralibarghamadi2008 <amiralibarghamadi851@gmail.com>
2026-09-16 15:07:09 +0330

    fix route for import file's

--- src/service/AuthService/SignInService/signInService.js
+++ src/service/AuthService/SignInService/signInService.js
@@ -1,7 +1,7 @@
 import { GetOtpCode , DeleteOtp } from "../../../repository/OtpRepository/OtpRepository.js";
 import {FindAllUser, FindUserByPhone, CreateUser} from "../../../repository/UserRepository/UserRepository.js"
-import AccessToken from "../../../utils/Tokens/AccessToken/accessToken.js";
-import RefreshToken from "../../../utils/Tokens/RefreshToken/refreshToken.js"
+import AccessToken from "../../../utils/tokens/AccessToken/accessToken.js";
+import RefreshToken from "../../../utils/tokens/RefreshToken/refreshToken.js"
 
 export default async function SignInService(userData) {
     try {
